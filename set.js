c12cfb68c1c9b1f744c4eac3a562c7f8cff83470
Allan256prog <richardbyaruhanga59@gmail.com>
2025-06-12 01:03:00 +0300

    Update set.js

--- set.js
+++ set.js
@@ -12,9 +12,9 @@ const databasePath = path.join(__dirname, './database.db');
 const DATABASE_URL = process.env.DATABASE_URL === undefined
     ? databasePath
     : process.env.DATABASE_URL;
-module.exports = { session: process.env.SESSION_ID || '',
+module.exports = { session: process.env.SESSION_ID || 'nexus~Vv1RSAja#3peJg0IeEJwlP44f8qkMazgszVkxcc1s6sM6XW72gXc',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "254710772666",
+    OWNER_NAME: process.env.OWNER_NAME || "256788501042",
     NUMERO_OWNER : process.env.NUMERO_OWNER || " Ibrahim Adams",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
